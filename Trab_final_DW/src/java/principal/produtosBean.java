32c17396457ffa36181c029de391af8addd120be
Isaac <isaacbarros88@gmail.com>
2019-07-18 10:33:56 -0300

    Adição das imagens e nomes dos produtos

--- Trab_final_DW/src/java/principal/produtosBean.java
+++ Trab_final_DW/src/java/principal/produtosBean.java
@@ -24,12 +24,12 @@ public class produtosBean {
      */
     public produtosBean() {
         produtos = new ArrayList<>();
-        produtos.add(new Produto("imagem1", "Nome do Produto 1", "10,00", "01"));
-        produtos.add(new Produto("imagem2", "Nome do Produto 2", "20,00", "02"));
-        produtos.add(new Produto("imagem3", "Nome do Produto 3", "30,00", "03"));
-        produtos.add(new Produto("imagem4", "Nome do Produto 4", "40,00", "04"));
-        produtos.add(new Produto("imagem5", "Nome do Produto 5", "50,00", "05"));
-        produtos.add(new Produto("imagem6", "Nome do Produto 6", "60,00", "06"));        
+        produtos.add(new Produto("imagem1", "Ovo vegano 200G", "29,90", "0"));
+        produtos.add(new Produto("imagem2", "Maionese Hellmann's vegana 250g", "12,40", "1"));
+        produtos.add(new Produto("imagem3", "Filés de soja sabor carne vermelha", "10,00", "2"));
+        produtos.add(new Produto("imagem4", "Carne moída vegetal superbom 400g", "21,90", "3"));
+        produtos.add(new Produto("imagem5", "Almôndega vegetariana ao molho superbom", "24,90", "4"));
+        produtos.add(new Produto("imagem6", "Massa talharim cenoura casarão sem glúten 300g", "14,00", "5"));        
     }
     
     public List<Produto> getProdutos(){
