34137431b41039ce6243597e9394b37ebd1a18da
Hatake Attack <fernando.freire.94@hotmail.com>
2019-07-17 23:23:20 -0300

    Adicionado sistema de carrinho de compras (incompleto)

--- Trab_final_DW/src/java/principal/carrinhoBean.java
+++ Trab_final_DW/src/java/principal/carrinhoBean.java
@@ -5,6 +5,7 @@
  */
 package principal;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -13,15 +14,26 @@ import javax.faces.bean.RequestScoped;
  *
  * @author hatake-attack
  */
-@ManagedBean
+@ManagedBean(name = "carrinhoBean")
 @RequestScoped
 public class carrinhoBean {
     
+    private List <Produto> carrinho;
+    
+    public carrinhoBean() {
+        carrinho = new ArrayList<>();
+    }
+
+    public List<Produto> getCarrinho() {
+        return carrinho;
+    }
+    
+    public void addProduto(Produto produto){
+        carrinho.add(produto);
+    }
 
     /**
      * Creates a new instance of carrinhoBean
      */
-    public carrinhoBean() {
-    }
     
 }
