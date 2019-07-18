ea8f732670c11f49c884f0e4f5a4bd8bde8a3ec1
Hatake Attack <fernando.freire.94@hotmail.com>
2019-07-17 21:49:20 -0300

    Modificações da grade de produtos

---
+++ Trab_final_DW/src/java/principal/carrinhoBean.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author hatake-attack
+ */
+@ManagedBean
+@RequestScoped
+public class carrinhoBean {
+    
+
+    /**
+     * Creates a new instance of carrinhoBean
+     */
+    public carrinhoBean() {
+    }
+    
+}
